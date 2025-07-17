4c4f9b4160a80fdb0b1774f35a4b90ca079a2259
daveslade1 <160166091+daveslade1@users.noreply.github.com>
2025-07-17 14:19:45 +0100

    Update panels.ts

--- src/data/panels.ts
+++ src/data/panels.ts
@@ -3,19 +3,19 @@ export const panels = [
   {
     title: "ADULTS CLASSES",
     subtitle: "Striking for Self Defence",
-    image: "https://jamiemarsland.co.uk/wp-content/uploads/2025/02/photo-32-scaled.jpg",
+    image: "https://defend-km.co.uk/wp-content/uploads/2023/03/cambridge-krav-maga-self-defence1.webp",
     path: "/photo-journal"
   },
   {
     title: "TEENS CLASSES",
     subtitle: "Self Defence for ages 11-15",
-    image: "https://jamiemarsland.co.uk/wp-content/uploads/2025/02/photo-18-scaled.jpg",
+    image: "https://defend-km.co.uk/wp-content/uploads/2023/03/cambridge-krav-maga-self-defence4.webp",
     path: "/iphone-sketches"
   },
   {
     title: "WOMENS KICKBOXING",
     subtitle: "Women Only Classes",
-    image: "/lovable-uploads/0f7521e1-9769-426b-a02f-924c23a17cbb.png",
+    image: "https://defend-km.co.uk/wp-content/uploads/2024/02/Kickboxing-women-classes-cambridge.jpg",
     path: "/sketches"
   },
 ];
