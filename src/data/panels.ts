ff9641b8cd5a8f67d5bbd6838f2e2e14630ce711
daveslade1 <160166091+daveslade1@users.noreply.github.com>
2025-07-17 14:17:49 +0100

    Update panels.ts

--- src/data/panels.ts
+++ src/data/panels.ts
@@ -18,30 +18,6 @@ export const panels = [
     image: "/lovable-uploads/0f7521e1-9769-426b-a02f-924c23a17cbb.png",
     path: "/sketches"
   },
-  {
-    title: "PHOTOS",
-    subtitle: "my favourites",
-    image: "https://jamiemarsland.co.uk/wp-content/uploads/2025/03/Screenshot-2025-03-06-at-22.16.46.png",
-    path: "/photos"
-  },
-  {
-    title: "PAINTINGS",
-    subtitle: "experiments",
-    image: "https://jamiemarsland.co.uk/wp-content/uploads/2024/06/cliff.jpg",
-    path: "/paintings"
-  },
-  {
-    title: "TRAVELS",
-    subtitle: "world map",
-    image: "https://jamiemarsland.co.uk/wp-content/uploads/2025/02/photo-29-scaled.jpg",
-    path: "/world-map"
-  },
-  {
-    title: "WRITINGS",
-    subtitle: "my thoughts",
-    image: "https://images.unsplash.com/photo-1455390582262-044cdead277a",
-    path: "/writings"
-  },
 ];
 
 // Update menu items to use the exact panel titles
